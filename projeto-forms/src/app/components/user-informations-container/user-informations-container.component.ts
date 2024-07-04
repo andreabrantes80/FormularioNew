0d4ff69eada094d31f7891dc4c950af0ab1edb88
andreabrantes80 <meloabrantes@gmail.com>
2024-07-03 21:31:45 -0300

    Criação do controller user-informations

--- projeto-forms/src/app/components/user-informations-container/user-informations-container.component.ts
+++ projeto-forms/src/app/components/user-informations-container/user-informations-container.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IUser } from '../../interfaces/user/user-interface';
+import { UserFormController } from './user-form-controller';
 
 
 @Component({
@@ -7,13 +8,20 @@ import { IUser } from '../../interfaces/user/user-interface';
   templateUrl: './user-informations-container.component.html',
   styleUrls: ['./user-informations-container.component.scss']
 })
-export class UserInformationsContainerComponent implements OnChanges{
+export class UserInformationsContainerComponent extends UserFormController implements OnChanges{
   currentTabIndex: number = 0;
 
   @Input() userSelected: IUser = {} as IUser;
   @Input() isInEditMode: boolean = false;
 
-  ngOnChanges(_: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges) {
     this.currentTabIndex = 0;
+
+    const HAS_USER_SELECTED =
+      changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
+
+    if (HAS_USER_SELECTED) {
+      this.fullFillUserForm(this.userSelected);
+    }
   }
 }
