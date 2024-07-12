9d02c3b24150db30e0e8695aaf89fac5892e8f78
andreabrantes80 <meloabrantes@gmail.com>
2024-07-11 22:28:03 -0300

    Criação do método de filtro do campo país

--- projeto-forms/src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ projeto-forms/src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,21 +1,55 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CountriesList } from '../../types/countries-list';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-general-informations-edit',
   templateUrl: './general-informations-edit.component.html',
-  styleUrls: ['./general-informations-edit.component.scss']
+  styleUrls: ['./general-informations-edit.component.scss'],
 })
-export class GeneralInformationsEditComponent implements OnChanges {
+export class GeneralInformationsEditComponent implements OnInit, OnChanges {
+  countriesListFiltered: CountriesList = [];
   @Input() userForm!: FormGroup;
   @Input() countriesList: CountriesList = [];
 
-  ngOnChanges(changes: SimpleChanges){
-    console.log(changes);
+  ngOnInit() {
+    this.watchCountryFormChangesAndFilter();
   }
 
-  get emailControl(): FormControl{
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    this.countriesListFiltered = this.countriesList;
+  }
+
+  get emailControl(): FormControl {
     return this.userForm.get('generalInformations.email') as FormControl;
+  }
+  get countryControl(): FormControl {
+    return this.userForm.get('generalInformations.country') as FormControl;
+  }
+
+  onCountrySelected(event: MatAutocompleteSelectedEvent) {
+    console.log(event);
+  }
+
+  //método bind recebe a classe como parametro e faz a referencia com a classe
+  private watchCountryFormChangesAndFilter() {
+    // this.countryControl.valueChanges.subscribe((value: string) => this.filterCountriesList(value));
+    this.countryControl.valueChanges.subscribe(
+      this.filterCountriesList.bind(this)
+    );
+  }
+  //Resultado do filtro vou guardar em uma nova lista filtrada
+  private filterCountriesList(searchTerm: string) {
+    this.countriesListFiltered = this.countriesList.filter((country) =>
+      country.name.toLowerCase().includes(searchTerm.toLowerCase().trim())
+    );
   }
 }
