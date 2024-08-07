36742d3adc334edce5249f38e6117aa514fc6e2b
andreabrantes80 <meloabrantes@gmail.com>
2024-08-06 22:04:59 -0300

    Implementada a lógica para aplicar as máscaras dinâmicas

--- projeto-forms/src/app/components/contact-informations-edit/components/phone-list-edit/phone-list-edit.component.ts
+++ projeto-forms/src/app/components/contact-informations-edit/components/phone-list-edit/phone-list-edit.component.ts
@@ -1,3 +1,4 @@
+import { PhoneTypeEnum } from './../../../../enums/phone-type.enum';
 import { Component, Input } from '@angular/core';
 import { FormArray, FormGroup } from '@angular/forms';
 
@@ -11,5 +12,14 @@ export class PhoneListEditComponent {
 
   get phoneList(): FormArray{
     return this.userForm.get('contactInformations.phoneList') as FormArray;
+  }
+
+  getPhoneMask(phoneType: number) {
+    const phoneMaskMap: {[key in PhoneTypeEnum]: string} = {
+      [PhoneTypeEnum.RESIDENTIAL]: '+00 00 0000-0000',
+      [PhoneTypeEnum.MOBILE]: '+00 00 00000-0000',
+      [PhoneTypeEnum.EMERGENCY]: '+00 00 00000-0000||+00 00 0000-0000',
+    };
+    return phoneMaskMap[phoneType as PhoneTypeEnum];
   }
 }
